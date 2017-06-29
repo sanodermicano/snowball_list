dcf6fa9da1103637c45d1d1e1040a4f25d1daba3
sanodermicano <pvmsano@gmail.com>
2017-06-29 20:57:25 +0300

    deleted target

--- snowball_list/target/debug/snowball_list.d
+++
@@ -1,1 +0,0 @@
-/home/sano/Rust_Projects/snowball_list/snowball_list/target/debug/snowball_list: /home/sano/Rust_Projects/snowball_list/snowball_list/src/date.rs /home/sano/Rust_Projects/snowball_list/snowball_list/src/main.rs /home/sano/Rust_Projects/snowball_list/snowball_list/src/task.rs
